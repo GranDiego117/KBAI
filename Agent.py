15e860851a23c429cddb8c5e0dcde0ebad2bffcd
Diego Granados <diegodavid.granados@gmail.com>
2023-05-26 17:49:18 -0700

    early mod

--- Agent.py
+++ Agent.py
@@ -9,8 +9,8 @@
 # These methods will be necessary for the project's main method to run.
 
 # Install Pillow and uncomment this line to access image processing.
-#from PIL import Image
-#import numpy
+from PIL import Image
+import numpy as np
 
 class Agent:
     # The default constructor for your Agent. Make sure to execute any
@@ -31,4 +31,40 @@ class Agent:
     # Make sure to return your answer *as an integer* at the end of Solve().
     # Returning your answer as a string may cause your program to crash.
     def Solve(self,problem):
-        return -1
+        
+        figure_a = problem.figures["A"]
+        #image_path = figure_a.visualFilename
+        problem_type = problem.problemType
+        problem_hasVisual = problem.hasVisual
+        problem_hasVerbal = problem.hasVerbal
+
+        problem_images = {}
+        option_images = {}
+
+        # Loop over each figure in the problem.
+        for name, figure in problem.figures.items():
+            # Load the image file.
+            image_path = figure.visualFilename
+            image = Image.open(image_path)
+
+            if name.isalpha():
+                problem_images[name] = image
+            elif name.isdigit():
+                option_images[name] = image
+            
+            #print(f"Figure name: {name}, visualFilename: {figure.visualFilename}, problemType: {problem.problemType}")
+        
+        problem_arrays = [np.array(image) for image in problem_images.values()]
+        are_problem_images_identical = all(np.array_equal(problem_arrays[0], prompt_array) for prompt_array in problem_arrays)
+
+        print(f"Are all prompt images identical? {are_problem_images_identical}")
+
+        if are_problem_images_identical:
+            for option_name, option_image in option_images.items():
+                if np.array_equal(problem_arrays[0], np.array(option_image)):
+                    print(f"Option {option_name} is identical to the problem images.")
+                    return int(option_name)
+
+
+        #print("Path: ", image_path, " type: ", problem_type, " visual: ", problem_hasVisual, " verbal: ", problem_hasVerbal)
+        return 1
