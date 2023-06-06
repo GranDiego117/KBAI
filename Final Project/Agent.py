0446e178eff1aa0a2e976e7d75042df9def9f774
Diego Granados <diegodavid.granados@gmail.com>
2023-06-05 20:00:10 -0700

    test with pillow

--- Final Project/Agent.py
+++ Final Project/Agent.py
@@ -9,8 +9,9 @@
 # These methods will be necessary for the project's main method to run.
 
 # Install Pillow and uncomment this line to access image processing.
-from PIL import Image
+from PIL import Image, ImageOps
 import numpy as np
+import cv2 as cv
 
 class Agent:
     # The default constructor for your Agent. Make sure to execute any
@@ -47,23 +48,88 @@ class Agent:
             image_path = figure.visualFilename
             image = Image.open(image_path)
 
+            # If the name is a letter, add it to the problem images. 
             if name.isalpha():
                 problem_images[name] = image
+            # If the name is a number, add it to the options images.
             elif name.isdigit():
                 option_images[name] = image
-            
-            #print(f"Figure name: {name}, visualFilename: {figure.visualFilename}, problemType: {problem.problemType}")
-        
-        problem_arrays = [np.array(image) for image in problem_images.values()]
-        are_problem_images_identical = all(np.array_equal(problem_arrays[0], prompt_array) for prompt_array in problem_arrays)
 
-        print(f"Are all prompt images identical? {are_problem_images_identical}")
+        # If problem is 2x2
+        if problem.problemType == "2x2":
+            # Extract the individual images
+            A = problem_images.get('A')
+            B = problem_images.get('B')
+            C = problem_images.get('C')
 
-        if are_problem_images_identical:
-            for option_name, option_image in option_images.items():
-                if np.array_equal(problem_arrays[0], np.array(option_image)):
-                    print(f"Option {option_name} is identical to the problem images.")
-                    return int(option_name)
+            # Variables to store comparison results
+            AB_horizontal = AC_horizontal = AB_vertical = AC_vertical = 0
+            AB_rotation = AC_rotation = [0, 0, 0]  # For 90, 180, 270 degrees respectively
+
+            # Check for horizontal reflection similarity between A and B
+            if np.array_equal(np.array(A), np.array(ImageOps.mirror(B))):
+                AB_horizontal = 1
+
+            # Check for horizontal reflection similarity between A and C
+            if np.array_equal(np.array(A), np.array(ImageOps.mirror(C))):
+                AC_horizontal = 1
+                
+            # Check for vertical reflection similarity between A and B
+            if np.array_equal(np.array(A), np.array(ImageOps.flip(B))):
+                AB_vertical = 1
+
+            # Check for vertical reflection similarity between A and C
+            if np.array_equal(np.array(A), np.array(ImageOps.flip(C))):
+                AC_vertical = 1
+
+            # Check for rotation similarity
+            for i, degree in enumerate([90, 180, 270]):
+                rotated_B = B.rotate(degree)
+                rotated_C = C.rotate(degree)
+                if np.array_equal(np.array(A), np.array(rotated_B)):
+                    AB_rotation[i] = 1
+                if np.array_equal(np.array(A), np.array(rotated_C)):
+                    AC_rotation[i] = 1
+
+            # Find contours in the images
+            contours1, _ = cv.findContours(cv.cvtColor(np.array(A), cv.COLOR_BGR2GRAY), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+            contours2, _ = cv.findContours(cv.cvtColor(np.array(B), cv.COLOR_BGR2GRAY), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+            contours3, _ = cv.findContours(cv.cvtColor(np.array(C), cv.COLOR_BGR2GRAY), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+            # Check if contours are similar between A and B
+            AB_contour = cv.matchShapes(contours1[0], contours2[0], 1, 0.0)
+            # Check if contours are similar between A and C
+            AC_contour = cv.matchShapes(contours1[0], contours3[0], 1, 0.0)
+
+            # Create a list to hold the sequence of transformations that would take B to A and C to A
+            transformations_to_apply = []
+
+            # Determine the transformations to apply
+            if AB_horizontal == 1:
+                transformations_to_apply.append('Horizontal Reflection between A and B')
+            if AC_horizontal == 1:
+                transformations_to_apply.append('Horizontal Reflection between A and C')
+            if AB_vertical == 1:
+                transformations_to_apply.append('Vertical Reflection between A and B')
+            if AC_vertical == 1:
+                transformations_to_apply.append('Vertical Reflection between A and C')
+            for i, degree in enumerate([90, 180, 270]):
+                if AB_rotation[i] == 1:
+                    transformations_to_apply.append(f'Rotation by {degree} degrees between A and B')
+                if AC_rotation[i] == 1:
+                    transformations_to_apply.append(f'Rotation by {degree} degrees between A and C')
+            if AB_contour == 1:
+                transformations_to_apply.append('Same contours between A and B')
+            if AC_contour == 1:
+                transformations_to_apply.append('Same contours between A and C')
+
+
+            print(image_path, 'To find the image for quadrant D, apply these transformations: ', transformations_to_apply)
+
+
+        # If problem is 3x3
+        #elif problem.problemType == "3x3":
+            #print('This is a 3x3 problem')
 
 
         #print("Path: ", image_path, " type: ", problem_type, " visual: ", problem_hasVisual, " verbal: ", problem_hasVerbal)
